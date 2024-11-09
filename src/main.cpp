d0377f156e2824c00ebc20cab2eeac75ba3a068b
Anoof Chappangathil <anoof7262@gmail.com>
2024-11-09 14:33:00 +0400

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#define DEBUG 0
+
+#define PIN1 D1
+#define PIN2 D2      
+#define PIN3 D5   
+#define PIN4 D6     
+#define DEBOUNCE_DELAY 500
+#include <Arduino.h>
+
+uint32_t lastDebounceTime = 0;
+void setup() {
+  Serial.begin(9600);
+  pinMode(PIN1, INPUT_PULLUP);
+  pinMode(PIN2, INPUT_PULLUP);
+  pinMode(PIN3, INPUT_PULLUP);
+  pinMode(PIN4, INPUT_PULLUP);
+}
+
+void loop() {
+  if (digitalRead(PIN1) == LOW) {
+    if (millis() - lastDebounceTime < DEBOUNCE_DELAY) { return;}
+    Serial.println('A');
+    lastDebounceTime = millis();
+  }
+  if (digitalRead(PIN2) == LOW) {
+    if (millis() - lastDebounceTime < DEBOUNCE_DELAY) { return;}
+    Serial.println('B');
+    lastDebounceTime = millis();
+  }
+  if (digitalRead(PIN3) == LOW) {
+    if (millis() - lastDebounceTime < DEBOUNCE_DELAY) { return;}
+    Serial.println('C');
+    lastDebounceTime = millis();
+  }
+  if (digitalRead(PIN4) == LOW) {
+    if (millis() - lastDebounceTime < DEBOUNCE_DELAY) { return;}
+    Serial.println('D');
+    lastDebounceTime = millis();
+  }
+}
